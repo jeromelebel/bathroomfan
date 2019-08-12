6bd416c908ce7b3351eb5afaad29b718a1fc1ec7
Jérôme Lebel <jeromelebel@users.noreply.github.com>
2019-08-12 23:19:35 +0200

    Nicer code to set the fan speed according to the humidity

--- sources/VMC.ino
+++ sources/VMC.ino
@@ -19,6 +19,18 @@ int counter = 0;
 int loopDuration = 0;
 String startTime;
 
+typedef struct {
+  int speed;
+  int humidity;
+} HumidityFanSpeed;
+
+HumidityFanSpeed humidityFanSpeed[] = {
+  { 20, 50 },  // 20% fan speed, from  0% to  50% humidity.
+  { 40, 65 },  // 40% fan speed, from 50% to  65% humidity.
+  { 75, 100 }, // 75% fan speed, from 65% to 100% humidity.
+};
+const size_t humidityFanSpeedCount = sizeof(humidityFanSpeed) / sizeof(*humidityFanSpeed);
+
 void setup() {
   Serial.begin(115200);
   WiFi.setHostname("VMC");
@@ -60,14 +72,16 @@ void loop() {
   lightController.loop();
   ledController.loop();
   pirController.loop();
-  if (dhtController.getHumidity() < 50) {
-    setFanSpeed(20);
-  } else if (dhtController.getHumidity() < 51) {
-  } else if (dhtController.getHumidity() < 65) {
-    setFanSpeed(40);
-  } else if (dhtController.getHumidity() < 66) {
-  } else {
-    setFanSpeed(75);
+  int humidity = dhtController.getHumidity();
+  int currentFanSpeed = fanController.getFanSpeed();
+  for (int i = 0; i < humidityFanSpeedCount; i++) {
+    if (humidity <= humidityFanSpeed[i].humidity + 1 && currentFanSpeed == humidityFanSpeed[i].speed) {
+      break;
+    }
+    if (humidity < humidityFanSpeed[i].humidity) {
+      setFanSpeed(humidityFanSpeed[i].speed);
+      break;
+    }
   }
   if (pirController.isHumanPresent()) {
     ledController.setPixels(LEDController::PixelColors::percentageValue(dhtController.getHumidity(), 9, 99, 0x010000));
